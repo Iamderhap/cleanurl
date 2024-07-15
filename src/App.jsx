a595821f00f85ce0732736d91c3d220a52fb263f
moridiyah <moridiyahs@gmail.com>
2024-07-15 17:08:23 +0100

    contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import Api from "./Components/Api"
 import {Route, BrowserRouter, Routes} from "react-router-dom"
 import ErrorPage from "./Components/Error"
+import Contact from './Components/contact'
 
 function App() {
-  
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        {/* <Route index element={<Home/>} /> */}
-        <Route path="/api" element={<Api/>} />
-
-
-
-        {/* Should be last route */}
-        <Route path="*" element={<ErrorPage/>} />
-      </Routes>
+        <Routes>
+          {/* <Route index element={<Home/>} /> */}
+          <Route path='/api' element={<Api />} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path='*' element={<ErrorPage />} />
+        </Routes>
       </BrowserRouter>
     </>
   )
 }
 
 export default App
+
+
+        {/* Should be last route */}
+     
