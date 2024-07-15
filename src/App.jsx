f94d79a05fe0e1811c38bc397e1a1f291800d660
Hardeywarlex <princeabdulsamadadewale@gmail.com>
2024-07-15 17:01:45 +0100

    privacy page

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,25 @@ import Api from "./Components/Api"
 import {Route, BrowserRouter, Routes} from "react-router-dom"
 import ErrorPage from "./Components/Error"
 import Report from "./Components/Report"
+import Privacy from "./Components/Privacy";
 
 function App() {
   
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        {/* <Route index element={<Home/>} /> */}
-        <Route path="/api" element={<Api/>} />
-        <Route path="/report" element={<Report/>} />
+        <Routes>
+          {/* <Route index element={<Home/>} /> */}
+          <Route path="/api" element={<Api />} />
+          <Route path="/report" element={<Report />} />
+          <Route path="/privacy" element={<Privacy />} />
 
-
-
-        {/* Should be last route */}
-        <Route path="*" element={<ErrorPage/>} />
-      </Routes>
+          {/* Should be last route */}
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
